443ef054516ddd4e7a2106cfe5d86557a6a82f65
MugdhaBhatnagar <mxbhatnagar@students.everettcc.edu>
2025-06-04 22:58:52 +0000

    saved

---
+++ main.py
@@ -0,0 +1,13 @@
+# create CSV file for the riddles
+# import the stuff needed
+import csv
+import random
+
+input_file = 'riddles.csv'
+# ask the player what difficulty they want
+
+# randomly generate a riddle from the difficulty level they chose
+# take input from the user
+# check if the answer is correct or wrong
+# if its correct, tell them they won
+# if its wrong, give them 2 more chances
