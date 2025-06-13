8c9a471c14d80bda8a7cbfbc9c46410de24aec71
mugdha-bhatnagar <mugdha.bhatnagar@studentssd.org>
2025-06-13 00:46:42 +0000

    Added some more code

--- main.py
+++ main.py
@@ -2,37 +2,36 @@
 import random
 import time
 import riddles
+import riddler
 
 # ask the player what difficulty they want
 a = 0
 while (a < 3):
-    print("Please select difficulty level.")
-    time.sleep(2)
+    print("Please select difficulty level for your riddle.")
+    time.sleep(1)
     x = input( "\n 1: Easy \n 2: Medium \n 3: Hard \n ")
     if (x == 1):
         y = random.choice(riddles.easy_riddle_questions)
         z = riddles.easy_riddle_answers(y)
+    elif(x == 1):
+        random_element = random.randint(0, len(riddles.easy_riddle_questions)-1)
+        y = riddles.easy_riddle_questions[random_element]
+        z = riddles.easy_riddle_answers[random_element]
+    elif (x == 2):
+        random_element = random.randint(0, len(riddles.medium_riddle_questions)-1)
+        y = riddles.medium_riddle_questions[random_element]
+        z = riddles.medium_riddle_answers[random_element]
+    elif (x == 3):
+        random_element = random.randint(0, len(riddles.hard_riddle_questions)-1)
+        y = riddles.hard_riddle_questions[random_element]
+        z = riddles.hard_riddle_answers[random_element] 
+    else:
+        print("I ask the riddles, not you. Goodbye.")
+        exit()
+    users_riddle = riddler.Riddler(x,y,z)
     print(y)
-elif:
+    time.sleep(1)
+    b = input("Guess the answer to the riddle. You have 3 tries.")
 # check if the answer is correct or wrong
 # if its correct, tell them they won
 # if its wrong, give them 2 more chances
-if (selectedDifficulty == 1):
-    random_element = random.randint(0, len(easy_riddle_questions)-1)
-    easy_riddle = easy_riddle_questions[random_element]
-    easy_answer = easy_riddle_answers[random_element]
-
-#  The print statement is there for me to see if the code is working
-    print(easy_riddle + easy_answer)
-
-# Remove the riddle so it doesn't get repeated
-    easy_riddle_questions.remove(random_element)
-    easy_riddle_answers.remove(random_element)
-
-elif (selectedDifficulty == 2):
-    random_element = random.randint(0, len(easy_riddle_questions)-1)
-    easy_riddle = easy_riddle_questions[random_element]
-    easy_answer = easy_riddle_answers[random_element]
-else:
-    print("I ask the riddles, not you. Goodbye.")
-    exit()
