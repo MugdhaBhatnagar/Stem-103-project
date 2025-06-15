d5a225b7590760cb34f896c75f4e80f7441758fb
mugdha-bhatnagar <mugdha.bhatnagar@studentssd.org>
2025-06-15 06:20:06 +0000

    some modularization

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
 # import the stuff needed
-import random
 import time
-import riddles
 import riddler
+import selection
 
 # ask the player what difficulty they want
 print("Please select difficulty level for your riddle.")
 time.sleep(1)
-x = input( "\n 1: Easy \n 2: Medium \n 3: Hard \n ")
-choice = int(x)
-if(choice == 1):
-    random_element = random.randint(0, len(riddles.easy_riddle_questions)-1)
-    y = riddles.easy_riddle_questions[random_element]
-    z = riddles.easy_riddle_answers[random_element]
-elif (choice == 2):
-    random_element = random.randint(0, len(riddles.medium_riddle_questions)-1)
-    y = riddles.medium_riddle_questions[random_element]
-    z = riddles.medium_riddle_answers[random_element]
-elif (choice == 3):
-    random_element = random.randint(0, len(riddles.hard_riddle_questions)-1)
-    y = riddles.hard_riddle_questions[random_element]
-    z = riddles.hard_riddle_answers[random_element]
-else:
-    print("I ask the riddles, not you. Goodbye.")
-    exit()
+choice =int(input( "\n 1: Easy \n 2: Medium \n 3: Hard \n "))
 
-users_riddle = riddler.Riddler(y,z)
-print(y)
-time.sleep(1)
-# check if the answer is correct or wrong
-a = 0
-while (a < 3):
-    b = input("Guess the answer to the riddle. You have 3 tries. ")
-    if (b != z):
-        time.sleep(1)
-        print("Ruh roh. You got it wrong, try again")
-        a +=1
-        # add code that prints the correct answer after the third try
-    else:
-        print("Yay! You solved the riddle!")
-        exit()
+
+riddle = select_riddle(choice)
+if riddle:
+    question, answer = riddle
+    game = Riddler(question, answer)
+    game.ask()
+else:
+    print("Invalid choice. Exiting.")
--- riddler.py
+++ riddler.py
@@ -1,5 +1,20 @@
-# Create classes (maybe?)
+import time
+
 class Riddler:
-    def __init__(self,riddle, answer):
+    def __init__(self,riddle, answer, max_attempts = 3):
         self.riddle = riddle
         self.answer = answer
+        self.max_attempts = max_attempts
+
+def ask(self):
+    print(self.riddle)
+    time.sleep(1)
+    for attempt in range(1, self.attempt + 1):
+        user_input = input(f"Enter your answer. You have {attempt}/{self.max_attempts}: remaining")
+        if (user_input == self.answer):
+            print("Yay! you solved the riddle!")
+            return True
+        else:
+            print("No, that's not it...")
+    print(f"You're out of luck. The correct answer was: {self.answer}")
+    return False
