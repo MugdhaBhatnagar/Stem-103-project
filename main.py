e6833a6e18f63e20b36fabe47ba2b7e19b267fd5
mugdha-bhatnagar <mugdha.bhatnagar@studentssd.org>
2025-06-15 06:24:04 +0000

    some more modularization

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # import the stuff needed
 import time
-import riddler
-import selection
-
+from selection import select_riddle
+from riddler import Riddler
 # ask the player what difficulty they want
 print("Please select difficulty level for your riddle.")
 time.sleep(1)
-choice =int(input( "\n 1: Easy \n 2: Medium \n 3: Hard \n "))
+difficulty =int(input( "\n 1: Easy \n 2: Medium \n 3: Hard \n "))
 
 
-riddle = select_riddle(choice)
+riddle = select_riddle(difficulty)
 if riddle:
     question, answer = riddle
     game = Riddler(question, answer)
