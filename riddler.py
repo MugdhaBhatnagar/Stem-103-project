7eecd67c8e5bdc3274d75add1441335930652758
mugdha-bhatnagar <mugdha.bhatnagar@studentssd.org>
2025-06-16 18:39:30 +0000

    code

--- riddler.py
+++ riddler.py
@@ -4,27 +4,29 @@ import random
 
 #  Sends the welcome statement and tells the player about the rules of the game
 def initialize_game():
-    print("Hello! Welcome to the Riddle Game! \nYou have to select a riddle difficulty and try to answer the riddle within 3 tries.")
+    print("Hello! Welcome to the Riddle Game! ")
+    time.sleep(1)
+    print("You have to select a riddle difficulty and try to answer the riddle within 3 tries.")
     time.sleep(1)
 
 # Selects a random riddle and its correct answer from the difficulty the player chose
 def select_riddle(difficulty):
     if(difficulty == 1):
         random_element = random.randint(0, len(riddles.easy_riddle_questions)-1)
-        y = riddles.easy_riddle_questions[random_element]
-        z = riddles.easy_riddle_answers[random_element]
+        ridd = riddles.easy_riddle_questions[random_element]
+        answer = riddles.easy_riddle_answers[random_element]
     elif (difficulty == 2):
         random_element = random.randint(0, len(riddles.medium_riddle_questions)-1)
-        y = riddles.medium_riddle_questions[random_element]
-        z = riddles.medium_riddle_answers[random_element]
+        ridd = riddles.medium_riddle_questions[random_element]
+        answer = riddles.medium_riddle_answers[random_element]
     elif (difficulty == 3):
         random_element = random.randint(0, len(riddles.hard_riddle_questions)-1)
-        y = riddles.hard_riddle_questions[random_element]
-        z = riddles.hard_riddle_answers[random_element]
+        ridd = riddles.hard_riddle_questions[random_element]
+        answer = riddles.hard_riddle_answers[random_element]
     else:
         print("I ask the riddles, not you. Goodbye.")
         exit()
-    return y,z
+    return ridd,answer
 
 # Takes input from the player and checks to see if its correct
 def ask(ridd,answer):
